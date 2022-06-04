9000157d4a686deb9364db4d8116a39f3401bb95
Akash Pal <creativeakashpal@gmail.com>
2022-06-04 20:23:49 +0530

    added error messages and registration successful alert box

--- script.js
+++ script.js
@@ -12,32 +12,71 @@ const phoneNumberError = document.getElementById('ph-error');
 const passwordError = document.getElementById('pw-error');
 const confirmPasswordError = document.getElementById('cpw-error');
 
-
-// firstName.addEventListener('invalid', function (event) {
-//     if (event.target.validity.valueMissing) {
-//       event.target.setCustomValidity('Please tell us how we should address you.');
-//     }
-//   })
-  
-//   firstName.addEventListener('change', function (event) {
-//     event.target.setCustomValidity('');
-//   })
-
 let validate = (el) => {
-    console.log(el);
-    if (undefined !== el.attributes['required'] && el.value.trim() == '') {
-        el.nextElementSibling.innerHTML = el.attributes['data-required-msg'] ? el.attributes['data-required-msg'].value : 'This field is required.';
+    if (undefined !== el.attributes['required'] && el.value.trim() == '')
+     {
+        el.nextElementSibling.textContent = `*PLEASE ENTER ${el.previousElementSibling.textContent}`
+        // el.nextElementSibling.innerText = el.attributes['data-required-msg'] ? el.attributes['data-required-msg'].value : '*This field is required.';
         return false;
     }
 
-    el.nextElementSibling.innerHTML = '';
+    el.nextElementSibling.innerText = '';
     return true;
 }
 
-document.querySelectorAll('input').forEach(function (el) {
-    el.addEventListener('keydown', function(e) {
-        validate(this);
-    });
+firstName.addEventListener('input', validateName);
+lastName.addEventListener('input', validateName);
+
+function validateName() {
+    if(/^[a-zA-Z]+$/.test(this.value) === false) {
+            this.nextElementSibling.textContent = 'please enter valid first name';
+            this.setCustomValidity("Invalid field.");
+        }
+        else {
+            this.nextElementSibling.textContent = '';
+            this.setCustomValidity("");
+        }
+}
+
+email.addEventListener('input', function() {
+    
+    if(email.validity.typeMismatch) {
+        emailError.textContent = 'please enter valid email address';
+    }
+    else {
+        emailError.textContent = '';
+    }
+});
+
+phoneNumber.addEventListener('input', function() {
+    if(phoneNumber.validity.patternMismatch) {
+        phoneNumberError.textContent = 'please enter valid phone number';
+    }
+    else {
+        phoneNumberError.textContent = '';
+    }
+});
+
+password.addEventListener('input', function() {
+    if(password.value.length < 8) {
+        passwordError.textContent = 'password is not strong enough';
+        password.setCustomValidity("Invalid field.");
+    }
+    else{
+        passwordError.textContent = '';
+        password.setCustomValidity("");
+    }
+})
+
+confirmPassword.addEventListener('input', function() {
+    if(password.value  !== confirmPassword.value) {
+        confirmPasswordError.textContent = "passwords do not match";
+        confirmPassword.setCustomValidity("Invalid field.");
+    }
+    else {
+        confirmPasswordError.textContent = '';
+        confirmPassword.setCustomValidity("");
+    }
 });
 
 document.getElementById('signup-form').onsubmit = function (e) {
@@ -48,5 +87,8 @@ document.getElementById('signup-form').onsubmit = function (e) {
         validated = !validated ? false : validate(el);
     });
 
-    if (validated) this.submit();
+    if (validated) {
+        alert('REGISTRATION SUCCESSFUL!!');
+        this.submit();
+    }
 }
